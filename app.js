3a74c61f34d66cbd3e69853df7d75edcaa69c68f
jessicatung <jessica.y.tung@gmail.com>
2014-08-15 20:02:27 -0700

    Added MVP skeleton Created init function Prototyped controller with yo

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("yo")
+window.addEventListener("load", init)
+
+function init(){
+	var controller = new Controller;
+	controller.yo()
+}
+
+function Controller(){};
+
+Controller.prototype = {
+	yo: function(){
+		console.log("something")
+	}
+}
+
+function Model(){
+
+};
+
+function View(){
+
+};
