33e62c6e11e0133acd9f8e33ce2e4a00f9cf54eb
jessicatung <jessica.y.tung@gmail.com>
2014-08-15 20:09:59 -0700

    Added event listener on keyup yo now fires upon keyup

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ window.addEventListener("load", init)
 
 function init(){
 	var controller = new Controller;
-	controller.yo()
+	controller.bindEventListeners()
 }
 
 function Controller(){};
 
 Controller.prototype = {
+	bindEventListeners: function(){
+		
+		document.addEventListener("keyup", this.yo)
+	},
 	yo: function(){
-		console.log("something")
+		console.log("yo")
 	}
 }
 
